678bf7c2d4b21d8a1b9e11cdd5620962d6cfa930
Ishwor Mali <ishwor.mali@eclipx.com>
2017-06-23 14:15:43 +1000

    implementation for selfhost identity server

--- IdServerSelfHost/IdServerSelfHost/Program.cs
+++ IdServerSelfHost/IdServerSelfHost/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,25 @@ using System.Threading.Tasks;
 
 namespace IdServerSelfHost
 {
+    using Microsoft.Owin.Hosting;
+    using Serilog;
+    using Server;
+
     class Program
     {
         static void Main(string[] args)
         {
+            Log.Logger=new LoggerConfiguration()
+                .WriteTo
+                .LiterateConsole(outputTemplate: "{Timestamp:HH:mm} [{Level}] ({Name:l}){NewLine} {Message}{NewLine}{Exception}")
+                .CreateLogger();
+
+            using (WebApp.Start<Startup>("http://localhost:5000"))
+            {
+                Console.WriteLine("server running.....");
+                Console.ReadLine();
+
+            }
         }
     }
 }
---
+++ IdServerSelfHost/IdServerSelfHost/Server/Scopes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdServerSelfHost.Server
+{
+    using IdentityServer3.Core.Models;
+    public class Scopes
+    {
+        public static List<Scope> Get()
+        {
+            return new List<Scope>
+            {
+                new Scope {Name="api1" }
+            };
+        }
+    }
+}
